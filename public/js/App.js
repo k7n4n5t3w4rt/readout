a365ddcdb672193983e36508343be74456679c02
Kynan Stewart <2020@mx.kynan.net.au>
2020-12-31 17:26:37 +1100

    Generated.

---
+++ public/js/App.js
@@ -0,0 +1,24 @@
+// @flow
+import { h } from "../web_modules/preact.js";
+import Dyad from "./Dyad.js";
+import Router from "../web_modules/preact-router.js";
+import htm from "../web_modules/htm.js";
+import { AppProvider } from "./AppContext.js";
+const html = htm.bind(h);
+
+/*::
+type Props = {
+  url: string
+};
+*/
+const App /*: function */ = (props /*: Props */) => {
+  return html`
+    <${AppProvider} >
+      <${Router} url="${props.url}">
+        <${Dyad} path="/" />
+      </${Router}>
+    </${AppProvider} >
+  `;
+};
+
+export default App;
